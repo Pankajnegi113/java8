5a8863c153eb6439cceee02bdb4664262e387506
3706021 <tarun.k@accolitedigital.com>
2022-09-22 09:51:05 +0530

    added processing operations for stream

--- Java8/src/main/java/com/java8/main/Streams.java
+++ Java8/src/main/java/com/java8/main/Streams.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Streams {
 
@@ -37,9 +38,27 @@ public class Streams {
         List<String>allProductList = products.stream().flatMap(List::stream).collect(Collectors.toList());
         System.out.println(allProductList);
 
+        //Processing using toArray - copying elements present in stream into an array
+        Stream<Integer> elemStream= Stream.of(10,50,90,30,20);
+        Integer[] array = elemStream.toArray(Integer[]::new);
+        for(Integer elem : array){
+            System.out.println(elem);
+        }
 
+        //Processing using count
+        elemStream =  Stream.of(10,50,90,30,20);
+        long countElem = elemStream.count();
+        System.out.println(countElem);
 
+        //Processing using sorted - natural sorting
+        elemStream =  Stream.of(10,50,90,30,20);
+        List<Integer>sortedElem = elemStream.sorted().collect(Collectors.toList());
+        System.out.println(sortedElem);
 
+        //sorting - customise sorting
+        elemStream =  Stream.of(10,50,90,30,20);
+        List<Integer>customSortedElem = elemStream.sorted((e1,e2)->-e1.compareTo(e2)).collect(Collectors.toList());
+        System.out.println(customSortedElem);
 
     }
 
