7e63dcd171712c5674264aa29277e9feee336db7
PankajSingh <pankaj.singh@accolitedigital.com>
2022-10-19 12:12:19 +0530

    code refactoring

--- Java8/src/main/java/com/multiThreading/main/CompletableFutureDemo.java
+++ Java8/src/main/java/com/multiThreading/main/CompletableFutureDemo.java
@@ -9,9 +9,11 @@ import java.util.concurrent.Executors;
 
 public class CompletableFutureDemo {
 
+    //runAsync - It takes Runnable Object and returns Void CompletableFuture
+    //supplyAsync - if want return type, takes Supplier<T> as input and return CompletableFuture<T>
     public static void main(String args[]) throws ExecutionException, InterruptedException {
 
-        //using runAsync(Runnable) , which uses fork pool for thread running
+        //using runAsync(Runnable) - void return type , which uses fork pool for thread running
         CompletableFuture completableFuture = CompletableFuture.runAsync(()->{
             System.out.println("Thread of runAsync method is: "+Thread.currentThread().getName());
             List<Integer> elements = Arrays.asList(10,20,30,40,50);
@@ -25,13 +27,32 @@ public class CompletableFutureDemo {
         //using runAsync(Runnable,Executor) , Using executor pool
         Executor executor = Executors.newFixedThreadPool(3);
         CompletableFuture completableFutureWithExecutor = CompletableFuture.runAsync(()->{
-            System.out.println("Thread of runAsync method is: "+Thread.currentThread().getName());
+            System.out.println("Thread of runAsync method with executor is: "+Thread.currentThread().getName());
             List<Integer> elements = Arrays.asList(10,20,30,40,50);
             elements.forEach(elem->System.out.print(elem+" "));
         },executor);
         completableFutureWithExecutor.get();
 
         System.out.println("Main thread");
+
+
+        //SupplyAsync
+
+        CompletableFuture<List<Integer>>response = CompletableFuture.supplyAsync(()->{
+            System.out.println("Thread of runAsync method is: "+Thread.currentThread().getName());
+            List<Integer> elements = Arrays.asList(10,20,30,40,50);
+            return elements;
+        });
+
+        System.out.println("Return from supplyAsync is :"+response.get());
+
+        CompletableFuture<List<Integer>>response2 = CompletableFuture.supplyAsync(()->{
+            System.out.println("Thread of runAsync method is: "+Thread.currentThread().getName());
+            List<Integer> elements = Arrays.asList(10,20,30,40,50);
+            return elements;
+        },executor);
+
+        System.out.println("Return from supplyAsync with executor thread pool is :"+response2.get());
 
     }
 
